084533ab5e9b1ceb54ce5462a76aa3eedf3d454f
Alan Tsui <alantsui2022@outlook.com>
2023-01-04 23:53:14 +0800

    Add Initializer for API Testing

--- reservation/src/test/java/com/book/reservation/ReservationApplicationTests.java
+++ reservation/src/test/java/com/book/reservation/ReservationApplicationTests.java
@@ -3,6 +3,10 @@ package com.book.reservation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ReservationApplicationTests {
 
