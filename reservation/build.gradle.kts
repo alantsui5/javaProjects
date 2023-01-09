1ee7cf94c8887318ea1784c4e75d6ee5c2868ccc
Alan Tsui <alantsui2022@outlook.com>
2023-01-09 17:23:19 +0800

    Declare SpringBoot V3 and Security V6

--- reservation/build.gradle.kts
+++ reservation/build.gradle.kts
@@ -13,18 +13,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.1")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.0.1")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.0.1")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.0.1")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.0.M1")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.0")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.1")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server:3.0.1")
 	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testImplementation("io.rest-assured:rest-assured")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.1")
+	testImplementation("org.springframework.security:spring-security-test:6.0.1")
+	testImplementation("io.rest-assured:rest-assured:5.3.0")
 }
 
 tasks.withType<Test> {
